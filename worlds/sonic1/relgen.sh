9665b0a729bbca38058c9c69137ceec1724aebe0
Kaithar <noreply.committer@the-cell.co.uk>
2025-02-08 12:59:40 +0000

    Small tweak

--- worlds/sonic1/relgen.sh
+++ worlds/sonic1/relgen.sh
@@ -2,5 +2,5 @@
 
 cd tracker
 zip ../sonic1.poptracker -r * -x images/raw/\*
-cd ..
-zip sonic1/sonic1.apworld -r sonic1 -x sonic1/__pycache__/\* sonic1/archived/\* sonic1/tracker/images/raw/\* sonic1/sonic1.apworld sonic1/sonic1.poptracker sonic1/relgen.sh
+cd ../..
+zip sonic1/sonic1.apworld -r sonic1 -x \*/__pycache__/\* sonic1/archived/\* sonic1/tracker/images/raw/\* sonic1/sonic1.apworld sonic1/sonic1.poptracker sonic1/relgen.sh sonic1/.gitignore
